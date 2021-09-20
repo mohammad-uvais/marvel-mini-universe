192fa5720bf180ec78d527879d1807f05d10a196
Mohammad Uvais <uvaismansoori@gmail.com>
2021-09-20 16:22:14 +0530

    Deployed App

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Contact from './contact.js';
 class App extends React.Component {
   render() {
      return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.mohammad-uvais.github.io }>
         <Navbar />
         <Switch>
            <Route exact path='/' component={Home}/>
